f21e9b9257f1cde45321e01b1e18a498e6e2ae87
LeticiaRutsatz <leticiarutsatz099@gmail.com>
2023-02-23 21:33:36 -0300

    ultimo commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import { routesApp } from "./config/routes";
 import "dotenv/config";
+import cors from "cors";
+
+const allowedOrigins = ["*"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
 app.use(express.json());
+app.use(cors(options));
 routesApp(app);
 
 app.listen(process.env.PORT, () =>
